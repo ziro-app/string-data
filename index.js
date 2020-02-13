e6dc3e9996508cfe2ba9741cd5920069fae37a08
unknown <ahmad.forhat@gmail.com>
2020-02-13 15:11:06 -0300

    change conditionals

--- index.js
+++ index.js
@@ -1,21 +1,14 @@
-const stringToData = dataSheets => {
-    if(typeof dataSheets !== 'string' || dataSheets === '' || dataSheets.indexOf('/') === -1 && dataSheets.indexOf('-') === -1){
-        return NaN
+const stringToDate = dateString => {
+    if(typeof dateString !== 'string') return false
+    if(dateString.includes('/')){
+        const [dia, mes, ano] = dateString.split('/')
+        return new Date(ano, mes - 1, dia)
     }
-    if(dataSheets.indexOf('/')){
-        const [dia, mes, ano] = dataSheets.split('/')
-        const data = new Date(ano, mes - 1, dia)
-        console.log(typeof dataSheets)
-        return data
+    if(dateString.includes('-')){
+        const [dia, mes, ano] = dateString.split('-')
+        return new Date(ano, mes - 1, dia)
     }
-    if(dataSheets.indexOf('-')){
-        const [dia, mes, ano] = dataSheets.split('-')
-        const data = new Date(ano, mes - 1, dia)
-        return data
-    }
-    if(new Date(dataSheets)){
-        return dataSheets
-    }
+    return false
 }
   
-module.exports = stringToData
+module.exports = stringToDate
